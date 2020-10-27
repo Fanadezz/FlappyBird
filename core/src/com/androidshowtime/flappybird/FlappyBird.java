4861cbb01678eaeeabb6d0abac84c68bc9e7cb08
Antony <vontonnie@gmail.com>
2020-10-27 08:05:32 +0300

    Add Background

--- core/src/com/androidshowtime/flappybird/FlappyBird.java
+++ core/src/com/androidshowtime/flappybird/FlappyBird.java
@@ -10,26 +10,35 @@ import sun.util.resources.cldr.gd.CalendarData_gd_GB;
 
 public class FlappyBird extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
-	
+
+	//textures in games creations are just image resources
+	Texture background;
+
+
+	//called when the app is run
 	@Override
 	public void create () {
+
+		//sprite is a collection of textures
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		background = new Texture("bg.png");
+
 	}
 
+
+	//called continuously in the app
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		//add background
+
+
+		//tells the render method we gonna start displaying sprites
 		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
 	}
 
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+
 	}
 }
