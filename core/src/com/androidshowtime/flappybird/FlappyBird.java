f828d7f001c2fea7761ba3a58137c63f127c35d1
Antony <vontonnie@gmail.com>
2020-10-27 08:34:52 +0300

    Draw Bird

--- core/src/com/androidshowtime/flappybird/FlappyBird.java
+++ core/src/com/androidshowtime/flappybird/FlappyBird.java
@@ -2,17 +2,15 @@ package com.androidshowtime.flappybird;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-
-import sun.util.resources.cldr.gd.CalendarData_gd_GB;
 
 public class FlappyBird extends ApplicationAdapter {
 	SpriteBatch batch;
 
 	//textures in games creations are just image resources
 	Texture background;
+	Texture bird;
 
 
 	//called when the app is run
@@ -22,6 +20,7 @@ public class FlappyBird extends ApplicationAdapter {
 		//sprite is a collection of textures
 		batch = new SpriteBatch();
 		background = new Texture("bg.png");
+		bird = new Texture("bird.png");
 
 	}
 
@@ -34,6 +33,25 @@ public class FlappyBird extends ApplicationAdapter {
 
 		//tells the render method we gonna start displaying sprites
 		batch.begin();
+
+		/*display we use batch.draw() - the draw method takes
+		* Texture - backgroung
+		* X-Co-ordinate - 0
+		* Y-Co-Ordinate - 0
+		* Width - we get the width from Gdx.graphics Library to match screen size
+		* Length - we get the length from Gdx.graphics Library*/
+
+
+		//draw background
+		batch.draw(background, 0,0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+
+		//draw bird at the center of the screen
+		batch.draw(bird, (Gdx.graphics.getWidth()/2f -  bird.getWidth()/2f),
+				   (Gdx.graphics.getHeight()/2f - bird.getHeight()/2f));
+
+		batch.end();
+
 	}
 
 	@Override
