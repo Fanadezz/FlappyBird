ca5bc072a644ab029c7583f57ad09b2ea9218554
Antony <vontonnie@gmail.com>
2020-10-28 19:10:50 +0300

    Restrict the bird from crossing the upper screen height

--- core/src/com/androidshowtime/flappybird/FlappyBird.java
+++ core/src/com/androidshowtime/flappybird/FlappyBird.java
@@ -6,99 +6,172 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class FlappyBird extends ApplicationAdapter {
-	SpriteBatch batch;
+    SpriteBatch batch;
 
-	//textures in games creations are just image resources
-	Texture background;
-	Texture []birds;
-	int flapState ;
+    //textures in games creations are just image resources
+    Texture background;
+    Texture[] birds;
+    int flapState;
 
-	//gravity variables
+    //gravity variables
 
-	float gravity = 0.2f;
-	float velocity = 0;
-	int manY = 0;
-
-
-	//called when the app is run
-	@Override
-	public void create () {
-
-		//sprite is a collection of textures
-		batch = new SpriteBatch();
-		background = new Texture("bg.png");
-		flapState = 0;
-		birds = new Texture[] {new Texture("bird.png"), new Texture("bird2.png")};
-
-	}
+    float gravity = 2;
+    float velocity = 0;
+    int birdYPosition = 0;
 
 
-	//called continuously in the app
-	@Override
-	public void render () {
-		//add background
+    //track game state
+    int gameState = 0;
 
 
-		//tells the render method we gonna start displaying sprites
-		batch.begin();
+    //called when the app is run
+    @Override
+    public void create() {
 
-		/*display we use batch.draw() - the draw method takes
-		* Texture - backgroung
-		* X-Co-ordinate - 0
-		* Y-Co-Ordinate - 0
-		* Width - we get the width from Gdx.graphics Library to match screen size
-		* Length - we get the length from Gdx.graphics Library*/
+        //sprite is a collection of textures
+        batch = new SpriteBatch();
+        background = new Texture("bg.png");
+        flapState = 0;
+        birds = new Texture[]{new Texture("bird.png"), new Texture("bird2.png")};
 
+        //set the initial Y Position for the bird at the first bird's height
+        birdYPosition = Gdx.graphics.getHeight() / 2 - birds[0].getHeight()/2 - 2;
 
-		//draw background
-		batch.draw(background, 0,0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-
-		//flap the bird
-		flapTheBird();
+    }
 
 
-		//set the initial Y Position for the bird
-		int birdPositionY = Gdx.graphics.getWidth()/2;
+    //called continuously in the app
+    @Override
+    public void render() {
+        //perform initial game set Up (draw background and flap wings)
+        initialGameSetUp();
 
-		//respond to touch
-		if (Gdx.input.isTouched()) {
+        //SCREEN TOUCHED - GAME ACTIVE
+        if (gameState != 0) {
 
-			Gdx.app.log("Touched", "Bird Touched");
-		}
-
-		batch.end();
-
-	}
-
-	private void flapTheBird() {
-		//flapState to switch between birds in birds array
-
-		if (flapState ==0) {
-
-			flapState = 1;
-
-		} else {
-
-			flapState = 0;
+            //respond to touch when screen is touched
+            if (Gdx.input.isTouched()) {
 
 
+                velocity = -20;
 
-		}
+                //if the screen is touched switch gameState
+                gameState = 1;
+
+            }
+
+            //perform gravity manoeuvres
+            performGravityManoeuvres();
+
+        }
+
+        //SCREEN NOT TOUCHED
+        else {
+            //respond to touch when screen is touched
+            if (Gdx.input.isTouched()) {
 
 
-		try {
-//slow the bird flaps
-			batch.draw(birds[flapState], (Gdx.graphics.getWidth() / 2f - birds[flapState].getWidth() / 2f),
-					   (Gdx.graphics.getHeight() / 2f - birds[flapState].getHeight() / 2f));
-			Thread.sleep(100);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
+                velocity = -20;
 
-	@Override
-	public void dispose () {
-		batch.dispose();
+                //if the screen is touched switch gameState
+                gameState = 1;
 
-	}
+            }
+
+
+        }
+
+
+
+
+    }
+
+
+    //make the bird flap
+
+    private void flapBirdWings() {
+        //flapState to switch between birds in birds array
+
+        if (flapState == 0) {
+
+            flapState = 1;
+
+        } else {
+
+            flapState = 0;
+
+
+        }
+
+
+        try {
+            //slow the bird flaps
+            batch.draw(birds[flapState],
+                       (Gdx.graphics.getWidth() / 2f - birds[flapState].getWidth() / 2f),
+                       (birdYPosition));
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private void performGravityManoeuvres() {
+
+
+        /*increase velocity of the bird each time the render-loop is called while
+         bringing down the bird by subtracting the velocity with each loop resulting
+         in bird falling faster and faster along the Y-Axis*/
+
+        //increasingly falling speed
+        velocity = velocity + gravity;
+
+
+        //subtracting the height will make bird shoot into the air
+        birdYPosition -= velocity;
+
+
+        if (birdYPosition <= 0 ) {
+
+            birdYPosition = 0;
+        }
+       //restrict bird from crossing the upper screen height
+        if (birdYPosition >= Gdx.graphics.getHeight() -birds[flapState].getHeight()){
+            birdYPosition= Gdx.graphics.getHeight() -birds[flapState].getHeight();
+
+        }
+
+
+    }
+
+    //initialize game and perform initial setups
+    private void initialGameSetUp() {
+
+        //tells the render method we gonna start displaying sprites
+        batch.begin();
+
+        /*display we use batch.draw() - the draw method takes
+         * Texture - background
+         * X-Co-ordinate - 0
+         * Y-Co-Ordinate - 0
+         * Width - we get the width from Gdx.graphics Library to match screen size
+         * Length - we get the length from Gdx.graphics Library*/
+
+
+        //draw background
+        batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+
+        //flap the bird
+        flapBirdWings();
+
+        batch.end();
+    }
+
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+
+    }
 }
